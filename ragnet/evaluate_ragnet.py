a0c4f71935c469fa8375fbda51c9949f3dfd26fc
Kunal Sinha <ksinha2@jagupard36.stanford.edu>
2025-11-10 20:49:20 -0800

    WIP and rename bi encoder

--- ragnet/evaluate_ragnet.py
+++ ragnet/evaluate_ragnet.py
@@ -1,15 +1,10 @@
 import torch
-import random
 import os
 import numpy as np
 import pandas as pd
 from torch.utils.data import DataLoader, Dataset
-from torch.cuda.amp import GradScaler
-from torch.optim import AdamW
-import torch.nn.functional as F
-from transformers import AutoModelForCausalLM, AutoTokenizer, get_linear_schedule_with_warmup
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from tqdm import tqdm
-from sklearn.metrics import accuracy_score
 import copy
 import argparse
 from dotenv import load_dotenv
@@ -17,37 +12,67 @@ from itertools import chain
 import json
 import wandb
 import time
-from consts import full_system_prompt
+from relation_retrieval.bi_encoder.run_bi_encoder import full_system_prompt
 
-from biencoder import BiEncoderModule
 BLANK_TOKEN = '[BLANK]'
 LLM_PAD_TOKEN = '[PAD]'
 MAX_RETRIES = 3
 load_dotenv()
 
+LLM_NAME = "meta-llama/Llama-2-7b-chat-hf"
+GENERATION_DATA_NAME = "data/WebQSP/generation/merged/WebQSP_test.json"
+RELATIONS_DATA_NAME = "data/WebQSP/relation_retrieval/candidate_relations/WebQSP_test_cand_rels_sorted.json"
 
-def evaluate(llm_model, llm_tokenizer, dataset):
+
+def load_data():
+    data = {}
+    with open(DATASET_NAME, "r") as f:
+        relations_data = json.load(f)
+    with open(f'data/WebQSP/generation/merged/WebQSP_{split}.json') as f:
+        generation_data_raw = json.loads(f.read())
+        generation_data = {
+            gen_data['ID']: gen_data
+            for gen_data in generation_data_raw
+        }
+    for current_id in generation_data:
+        data[current_id] = {
+            **generation_data[current_id],
+            "relations": relations_data[current_id]
+        }
+    return data
+
+
+def evaluate_single(llm_model, llm_tokenizer, question, relations):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    prompt = f"{full_system_prompt}\nQuestion: {question}\nRelevant relations: {relations}\nLogical form: "
+    inputs = llm_tokenizer(prompt, return_tensors="pt").to(device)
+    outputs = llm_model.generate(
+        **inputs
+    )
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(response)
     import pdb; pdb.set_trace()
 
-
-def load_llm_and_tokenizer(llm_name="meta-llama/Llama-2-7b-chat-hf"):
+    
+def load_llm_and_tokenizer():
     # New code for loading in LLM
-    device = "cuda" if torch.cuda.is_available() else "cpu"
     llm_token = os.getenv("HF_AUTH_TOKEN")
     print(f"Loading in LLM and tokenizer: {llm_name}...")
     before_mem = torch.cuda.memory_allocated()/1024**2 / 1000
     print(f"Before loading in LLM: {before_mem:.2f} GB of CUDA memory used")
     llm_model = AutoModelForCausalLM.from_pretrained(
-        llm_name,
+        LLM_NAME,
         torch_dtype=torch.bfloat16,
-        use_auth_token=llm_token
-    ).to(device)
+        use_auth_token=llm_token,
+        device_map="auto"
+    )
     after_mem = torch.cuda.memory_allocated()/1024**2 / 1000
     print(f"After loading in LLM: {after_mem:.2f} GB of CUDA memory used")
     print(f"Total LLM CUDA memory usage: {(after_mem - before_mem):.2f} GB")
     llm_tokenizer = AutoTokenizer.from_pretrained(
-        llm_name, use_fast=False,
-        use_auth_token=llm_token
+        LLM_NAME, use_fast=False,
+        use_auth_token=llm_token,
+        device_map="auto"
     )
     llm_tokenizer.add_special_tokens({"pad_token": LLM_PAD_TOKEN})
     llm_model.resize_token_embeddings(len(llm_tokenizer))
@@ -57,6 +82,9 @@ def load_llm_and_tokenizer(llm_name="meta-llama/Llama-2-7b-chat-hf"):
 
 def main():
     llm_model, llm_tokenizer = load_llm_and_tokenizer()
+    data = load_data()
+    for current_id, example in data.items():
+        evaluate(llm_model, llm_tokenizer, example["question"], example["relations"])
     
 
 if __name__ == "__main__":
