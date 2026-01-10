abb58cc41aa327a0c26d55f2f5c07fccc67f4598
Kunal Sinha <ksinha2@stanford.edu>
2026-01-10 15:35:44 -0800

    DO NOT change existing code - only wrappers

--- eval_topk_prediction_final.py
+++ eval_topk_prediction_final.py
@@ -169,7 +169,6 @@ def denormalize_s_expr_new(normed_expr,
                                         cur_seg = cur_seg.replace(", ",".")
                                     else:
                                         # view as relation
-                                        cur_seg = cur_seg.replace("  ", " ") # KUNAL added this
                                         cur_seg = cur_seg.replace(' , ',',')
                                         cur_seg = cur_seg.replace(',','.')
                                         cur_seg = cur_seg.replace(' ', '_')
